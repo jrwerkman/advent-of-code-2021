2d875303f6b8b637c3fd07ad7a12fa9366dd3a68
Jan <Jan@DESKTOP-FNA03AU.localdomain>
2023-03-25 13:17:37 +0100

    transform to binary

--- src/main/java/nl/jrwer/challenge/advent/day16/Decoder.java
+++ src/main/java/nl/jrwer/challenge/advent/day16/Decoder.java
@@ -1,15 +1,43 @@
 package nl.jrwer.challenge.advent.day16;
 
 public class Decoder {
-	final String sequence;
+	final String hexSequence;
+	final String binarySequence;
 	
 	int version; // first three bits
 	int typeId;  // second three bits
 	
 	
 	
-	public Decoder(String sequence) {
-		this.sequence = sequence;
+	public Decoder(String hexSequence) {
+		this.hexSequence = hexSequence;
+		this.binarySequence = convert();
+	}
+	
+	private String convert() {
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0; i<hexSequence.length(); i++) {
+			int digit = Integer.parseInt(hexSequence.substring(i, i+1), 16);
+			String binary = Integer.toBinaryString(digit);
+			
+			sb.append(padLeftZeros(binary, 4));
+		}
+		
+		return sb.toString();
+	}
+	
+	public String padLeftZeros(String inputString, int length) {
+	    if (inputString.length() >= length) {
+	        return inputString;
+	    }
+	    StringBuilder sb = new StringBuilder();
+	    while (sb.length() < length - inputString.length()) {
+	        sb.append('0');
+	    }
+	    sb.append(inputString);
+
+	    return sb.toString();
 	}
 	
 	public int sumVersionNumbers() {
